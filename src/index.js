fadbd1f4de283820d9c70558bf29a764a3817cdd
Manish Joshi <manish.joshi@ambarkaar.com>
2020-04-26 19:13:20 +0545

    added dynamic element for react element

--- src/index.js
+++ src/index.js
@@ -2,7 +2,62 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //Render dom 
+const name = 'Josh Perez';
+const user = {
+  firstName: 'James',
+  lastName: 'Roses'
+};
+const elementDiv = (
+  <div>
+    <h1>Hello!</h1>
+    <h2>Good to see you here.</h2>
+  </div>
+);
+
+const userAgent=false;
+
+function getUsers(user){
+  return user.firstName+'=='+user.lastName;
+}
+
+function getGreeting(userAgent) {
+  if (userAgent) {
+    return <h1>Hello, {getUsers(user)}!</h1>;
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
+
+const dynamicElement=React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Hello, world! from dynamic element'
+);
+
+
+// Note: this structure is simplified
+const elementwithdiffStructure = {
+  type: 'h1',
+  props: {
+    className: 'greeting',
+    children: 'Hello, world!'
+  }
+};
+
+const element = <h1>
+                  Hello, {name}
+                  <br/>
+                  {getUsers(user)}
+                  <br/>
+                  {getGreeting(userAgent)}
+                  <br/>
+                  {elementDiv}
+                  <br/>
+                 {dynamicElement}
+                 <br/>
+                 {/* {elementwithdiffStructure} */}
+                </h1>;
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  element,
   document.getElementById('root')
 );
