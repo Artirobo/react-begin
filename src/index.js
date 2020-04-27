ad83523fd4def35930d762b8329674880c30305c
Manish Joshi <manish.joshi@ambarkaar.com>
2020-04-27 10:29:47 +0545

    compose component and render the component from class

--- src/index.js
+++ src/index.js
@@ -43,6 +43,92 @@ const elementwithdiffStructure = {
   }
 };
 
+
+
+function tick() {
+  const element1 = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element1, document.getElementById('as'));
+}
+setInterval(tick, 1000);
+
+
+const elementwithDiv = <div />;
+
+const elementCustomComponent = <Welcome name="Sara" />;
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+//Extracting Components
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      
+      {/* <div className="UserInfo"> */}
+        {/* <img className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        /> */}
+        
+        
+        {/* <Avatar user={props.author} />
+
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div> */}
+
+        <UserInfo user={props.author} />
+
+
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
 const element = <h1>
                   Hello, {name}
                   <br/>
@@ -54,8 +140,37 @@ const element = <h1>
                   <br/>
                  {dynamicElement}
                  <br/>
-                 {/* {elementwithdiffStructure} */}
+
+                  {elementwithdiffStructure.type}
+                  {elementwithdiffStructure.props.children}
+                   {/* it is for tick function */}
+                  <h1 id="as">
+
+                  </h1>
+                  
+                  {elementwithDiv}
+
+
+                  {elementCustomComponent}
+                  {/* Composing Components */}
+                  <App/>
+                
+                  <Comment
+                          date={comment.date}
+                          text={comment.text}
+                          author={comment.author}
+                          />
                 </h1>;
+//Composing Components
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
 
 ReactDOM.render(
   element,
